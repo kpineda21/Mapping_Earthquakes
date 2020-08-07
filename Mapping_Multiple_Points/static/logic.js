6301007281243e9f5b2a426c0d7021a3c705d33a
Karen P <karenp@Karens-MacBook-Pro.local>
2020-08-06 19:35:11 -0700

    multiple points.

--- Mapping_Multiple_Points/static/logic.js
+++ Mapping_Multiple_Points/static/logic.js
@@ -18,13 +18,6 @@ cityData.forEach(function(city) {
 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
 });
-
-// // Add a circle to the map
-// L.circleMarker([34.0522, -118.2437],{
-// 	radius: 300,
-// 	color: "black",
-// 	fillColor: '#ffffa1'
-//  }).addTo(map);
  
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', { 
